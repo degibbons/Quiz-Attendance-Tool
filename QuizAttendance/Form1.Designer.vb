108836992c8fd9fbda0b034874d2b1504a6c26a6
Danny Gibbons <51105241+degibbons@users.noreply.github.com>
2020-08-25 16:43:21 -0400

    Visual Studio Files with .exe included

---
+++ QuizAttendance/Form1.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ofd_StudentList = New System.Windows.Forms.OpenFileDialog()
+        Me.ofd_QuizTakerList = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(24, 77)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(163, 64)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Select Student List"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(24, 163)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(163, 64)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Select Quiz Taker List"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(230, 77)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(470, 64)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Student List File Name"
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(233, 163)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(467, 64)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "List of Quiz Takers"
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(24, 250)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(163, 64)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Compare"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(212, 329)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(233, 164)
+        Me.ListBox1.TabIndex = 5
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 16
+        Me.ListBox2.Location = New System.Drawing.Point(467, 329)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(233, 164)
+        Me.ListBox2.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(195, 18)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(257, 36)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Quiz Takers Tool"
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(24, 420)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(102, 72)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "Help"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(212, 278)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(233, 48)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Students Who Have Not Taken the Quiz"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(500, 275)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(162, 51)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Students Who Scored Below 100"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'ofd_StudentList
+        '
+        Me.ofd_StudentList.FileName = "OpenFileDialog1"
+        Me.ofd_StudentList.Filter = "CSV Files|*.csv|All Files|*.*"
+        '
+        'ofd_QuizTakerList
+        '
+        Me.ofd_QuizTakerList.FileName = "OpenFileDialog2"
+        Me.ofd_QuizTakerList.Filter = "CSV Files|*.csv|All Files|*.*"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(712, 505)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ofd_StudentList As OpenFileDialog
+    Friend WithEvents ofd_QuizTakerList As OpenFileDialog
+End Class
---
+++ QuizAttendance/Form1.vb
@@ -0,0 +1,106 @@
+﻿Imports System.Text.RegularExpressions
+Imports System.Globalization
+Public Class Form1
+    Dim destination1 As String
+    Dim destination2 As String
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click_1(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ofd_StudentList.FileName = Label1.Text
+        If ofd_StudentList.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            Label1.Text = ofd_StudentList.FileName
+            destination1 = ofd_StudentList.FileName
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ofd_QuizTakerList.FileName = Label2.Text
+        If ofd_QuizTakerList.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            Label2.Text = ofd_QuizTakerList.FileName
+            destination2 = ofd_QuizTakerList.FileName
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Using MyReader1 As New Microsoft.VisualBasic.FileIO.TextFieldParser(destination1)
+
+            MyReader1.TextFieldType = Microsoft.VisualBasic.FileIO.FieldType.Delimited
+            MyReader1.Delimiters = New String() {","}
+            Dim currentRow1 As String
+                Dim currentRow2 As String()
+                Dim stringPattern_Name As New Regex("\s*[a-zA-Z'-]+,\s*[a-zA-Z'-]+\s*", RegexOptions.IgnoreCase)
+            Dim stringPattern_SID As New Regex("\s*(?<SIDresult>\w+([.-]?\w+)*)@", RegexOptions.IgnoreCase)
+            MyReader1.ReadLine()
+                While Not MyReader1.EndOfData
+                Try
+                    currentRow1 = MyReader1.ReadLine()
+                    Console.WriteLine(currentRow1)
+                    Dim matchesName As Match = stringPattern_Name.Match(currentRow1, RegexOptions.IgnoreCase)
+                    If matchesName.Success Then
+                        Console.WriteLine(matchesName.Value)
+                    End If
+                    Dim matchesSID As Match = stringPattern_SID.Match(currentRow1, RegexOptions.IgnoreCase)
+                    If matchesSID.Success Then
+                        Console.WriteLine(matchesSID.Groups("SIDresult"))
+                    End If
+                    Using MyReader2 As New Microsoft.VisualBasic.FileIO.TextFieldParser(destination2)
+                        MyReader2.TextFieldType = Microsoft.VisualBasic.FileIO.FieldType.Delimited
+                        MyReader2.Delimiters = New String() {","}
+                        MyReader2.ReadLine()
+                        Dim output_Name As New Regex(matchesName.Value)
+                        Dim output_SID As New Regex(matchesSID.Groups("SIDresult").ToString)
+                        Dim matchedID As Boolean = False
+                        While Not MyReader2.EndOfData
+                            currentRow2 = MyReader2.ReadFields()
+
+                            For Each currentField As String In currentRow2
+
+                                If Regex.IsMatch(currentField, output_SID.ToString, RegexOptions.IgnoreCase) Then
+                                    matchedID = True
+                                    If currentRow2(5) <> 100 Then
+                                        ListBox2.Items.Add(matchesName.Value & " - " & matchesSID.Groups("SIDresult").ToString)
+
+
+                                        Exit For
+                                    End If
+                                End If
+                            Next
+                            If ((MyReader2.EndOfData = True) And (matchedID = False)) Then
+                                ListBox1.Items.Add(matchesName.Value & " - " & matchesSID.Groups("SIDresult").ToString)
+                            End If
+                        End While
+                    End Using
+                Catch ex As Microsoft.VisualBasic.FileIO.MalformedLineException
+                    MsgBox("Error in reading something?")
+                    End Try
+                End While
+
+        End Using
+    End Sub
+
+    Private Sub ofd_QuizTakerList_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles ofd_QuizTakerList.FileOk
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        MsgBox("Student List Button allows you to select the list of students in the class.
+               Quiz Taker's Button allows you to select the list of people who have been recorded
+                taking the quiz and their corresponding scores. Hit the Compare button to compare the 
+                two files. Please Contact Dan Gibbons (631) 456-7733 
+                or dgibbo03@nyit.edu for further information or help.")
+    End Sub
+End Class
